6453cfadafafe3de1bc51313f21a7456bda43df1
diankita <114820366+diankita@users.noreply.github.com>
2023-06-30 19:12:45 +0200

    Set realtime database from Firebase

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://realtime-database-champions-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "endorsements")
+
+const inputFieldEl = document.querySelector("#input-field");
+const publishButtonEl = document.querySelector("#publish-button");
+const endorsementsListEl = document.querySelector("#endorsements-list");
+// const liEl = document.createElement("li")
+
+publishButtonEl.addEventListener("click", function() {
+  let inputValue = inputFieldEl.value
+  push(endorsementsInDB, inputValue)
+
+  clearInputFieldEl();
+})
+
+onValue(endorsementsInDB, function(snapshot) {
+  let endorsementsArray = Object.values(snapshot.val());
+
+  clearEndorsementsListEl()
+
+  for (let i = 0; i < endorsementsArray.length; i++) {
+    const endorsement = endorsementsArray[i];
+    displayEndorsement(endorsement)
+  }
+})
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function clearEndorsementsListEl() {
+  endorsementsListEl.innerHTML = "";
+}
+
+function displayEndorsement(value) {
+  endorsementsListEl.innerHTML += `<li>${value}</li>`
+  // liEl.textContent = value;
+  // endorsementsListEl.appendChild(liEl);
+}
