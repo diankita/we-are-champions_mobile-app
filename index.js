6cf6b253c4fa196b9ca704c46f7add7559a43a4e
dianasul <sulkova.diana@gmail.com>
2023-07-04 15:10:53 +0200

    Added functionality to create/ delete endorsements

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL:
-    "https://realtime-database-champions-default-rtdb.europe-west1.firebasedatabase.app/"
+    "https://realtime-database-champions-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const endorsementsInDB = ref(database, "endorsements")
+const endorsementsInDB = ref(database, "endorsements");
 
 const inputFieldEl = document.querySelector("#input-field");
 const publishButtonEl = document.querySelector("#publish-button");
 const endorsementsListEl = document.querySelector("#endorsements-list");
-// const pEl = document.createElement("p")
 
-publishButtonEl.addEventListener("click", function() {
-  let inputValue = inputFieldEl.value
-  push(endorsementsInDB, inputValue)
+publishButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(endorsementsInDB, inputValue);
 
   clearInputFieldEl();
-})
+});
 
-onValue(endorsementsInDB, function(snapshot) {
-  let endorsementsArray = Object.values(snapshot.val());
+onValue(endorsementsInDB, function (snapshot) {
+if (snapshot.exists()) {
+  let endorsementsArray = Object.entries(snapshot.val());
 
-  clearEndorsementsListEl()
+  clearEndorsementsListEl();
 
   for (let i = 0; i < endorsementsArray.length; i++) {
-    const endorsement = endorsementsArray[i];
-    displayEndorsement(endorsement)
+    let currentEndorsement = endorsementsArray[i];
+    let currentEndorsementID = currentEndorsement[0];
+    let currentEndorsementValue = currentEndorsement[1];
+
+    displayEndorsement(currentEndorsement);
   }
-})
+} else {
+    endorsementsListEl.innerHTML = "No endorsements yet 🙁";
+}
+} 
+);
 
 function clearInputFieldEl() {
   inputFieldEl.value = "";
@@ -41,8 +54,17 @@ function clearEndorsementsListEl() {
   endorsementsListEl.innerHTML = "";
 }
 
-function displayEndorsement(value) {
-  endorsementsListEl.innerHTML += `<p>${value}</p>`
-  // pEl.textContent = value;
-  // endorsementsListEl.appendChild(liEl);
+function displayEndorsement(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("p");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `endorsements/${itemID}`);
+
+    remove(exactLocationOfItemInDB);
+  });
+  endorsementsListEl.append(newEl);
 }
